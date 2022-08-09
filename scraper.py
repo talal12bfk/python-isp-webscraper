d13df60797230147b8b52e97cc56542acaeeb140
Talal Alrwas <85613292+talal12bfk@users.noreply.github.com>
2022-08-09 15:45:59 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 from getpass import getpass
 url = "https://view.sy/self-care/#/usage"
 
-r = requests.get(url, auth=('maherqr@view.sy', getpass()))
+r = requests.get(url, auth=('email', getpass()))
 
 # soup = BeautifulSoup(r.text, features='html.parser')
 # print(soup)
